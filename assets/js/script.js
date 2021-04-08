7b6baf5792bb0b0b5450e8cea9e8233a6e4be422
wadep <wadepfacer@gmail.com>
2021-04-07 18:15:42 -0600

    finished edit task feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+    
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -44,6 +44,53 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+
+// adding on click to UL element
+$(".list-group").on("click", "p", function(){
+  var text = $(this)
+  .text()
+  .trim();
+
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+  
+    $(this).replaceWith(textInput);
+    // styles text input box to highlight or draw focus
+    textInput.trigger('focus');
+});
+
+$(".list-group").on('blur', 'textarea', function(){
+  // get text areas current value
+  var text =$(this)
+    .val()
+    .trim();
+
+    // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+    // get the tasks position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // updating task array data
+  tasks[status][index].text = text;
+
+  saveTasks();  
+
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+    
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
+});
+
 
 
 
